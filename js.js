84088691b46c3be7d730feeb8105b9c012f497d6
maxravel <maxmaterniak@gmail.com>
2018-03-21 17:58:29 +0100

    problems with nav

--- js.js
+++ js.js
@@ -6,5 +6,33 @@ $(document).ready(function() {
     else {
       $('.nav').removeClass('change');
     }
+
+    let Y = pageYOffset;
+    console.log(Y);
+  
+    // if(Y>=0 && Y<756){
+    //   $('#m1').addClass('onto');
+    //   $('#m1').mouseover(function(e){
+    //   $('#m1').removeClass('onto');
+    //   })
+    //   $('#m1').mouseleave(function(e){
+    //   $('#m1').addClass('onto');
+    //   });
+    // }
+    // else{$('#m1').removeClass('onto');}
+    
+    // if(Y>756 && Y<1500){$('#m2').addClass('onto');}
+    // else{$('#m2').removeClass('onto');}
+    
+    // if(Y>1500 && Y<2300){$('#m3').addClass('onto');}
+    // else{$('#m3').removeClass('onto');}
+    
+    // if(Y>2300){$('#m4').addClass('onto');}
+    // else{$('#m4').removeClass('onto');}
+
   });
+  
+  // var wH = $(window).height();
+  // console.log(wH);
+
 });
